3f39483a30f66a174a7d7449108b8ebb3ce9ce14
Marcelo Moreira <marcelo_mmds@hotmail.com>
2026-07-31 10:32:07 -0300

    fix: improving the performance in the code and queries

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -38,8 +38,27 @@ export class ProductsService {
     return productsWithCategory;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne() {
+    // 1ª Consulta (O "1" do N+1): Busca todos os produtos do banco
+    const products = await this.productRepository.find();
+
+    // Consultas extras (O "N" do N+1):
+    // Para cada produto da lista, o código faz um novo SELECT individual no banco
+    const productsWithCategory = await Promise.all(
+      products.map(async (product) => {
+        // Dispara uma query por produto: SELECT * FROM category WHERE id = product.categoryId
+        const category = await this.categoryRepository.findOne({
+          where: { id: product.categoryId },
+        });
+
+        return {
+          ...product,
+          category, // Anexa a categoria ao produto
+        };
+      }),
+    );
+
+    return productsWithCategory;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
